cc3cdfa316ad93453e2ae029549341f413c1dcb4
Rusty Eddy <rusty@rustyeddy.com>
2020-08-18 20:25:35 -0700

    we are returning a very simple static web page

--- spa/main.go
+++ spa/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	http.HandleFunc("/", handleHome)
+	//http.Handle("/api/health", handleHealth)
+	//http.Handle("/", http.FileServer(http.Dir("/pub")))
+
+	http.ListenAndServe(":1233", nil)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+
+	index := defaultIndex()
+	fmt.Fprint(w, index)
+}
+
+func defaultIndex() string {
+	str := `
+<!doctype html>
+<html>
+  <head>
+    <title>Static</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <p>This is an extremly simple static web page (with bad spelling)</p>
+  </body>
+</html>`
+	return str
 }
---
+++ spa/web.go
@@ -0,0 +1,5 @@
+package main
+
+type Web struct {
+	Addr string
+}

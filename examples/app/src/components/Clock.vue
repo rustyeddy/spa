de122ec561f6f973746de47c2c9580dcfdf5f092
Rusty Eddy <rusty@rustyeddy.com>
2020-08-19 09:18:43 -0700

    added vue application and a single page static clock

---
+++ examples/app/src/components/Clock.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+
+          {{ datestr }}
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+ export default {
+     name: 'Clock',
+     data() {
+         return {
+             datetime: "10:10 10/10/2020"
+         }
+     },
+     computed: {
+         datestr() {
+             var time = new Date();
+             var timestr = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+             return timestr
+         }   
+     }
+ }
+</script>
--- examples/app/src/views/Home.vue
+++ examples/app/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Clock/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+ // @ is an alias to /src
+ import Clock from '@/components/Clock.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+ export default {
+     name: 'Home',
+     components: {
+         Clock
+     }
+ }
 </script>

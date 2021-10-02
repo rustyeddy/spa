e3455d680958511cc317334143d01eaea0ec3b75
Rusty Eddy <rusty@rustyeddy.com>
2021-10-02 09:32:49 -0700

    take a web page from cmd line

--- spa/main.go
+++ spa/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"net/http"
 
@@ -30,8 +31,33 @@ func init() {
 	flag.BoolVar(&config.Debug, "debug", false, "turn on debugging")
 	flag.StringVar(&config.Addr, "addr", ":8080", "address string to serve up")
 	flag.StringVar(&config.Pub, "pub", "www", "path of website to serve up")
+}
 
-	// Register our REST handler callbacks
+func main() {
+	flag.Parse()
+
+	if len(os.Args) > 1 {
+		config.Pub = os.Args[1]
+		log.Println("HTML directory has been set to ", config.Pub)
+	}
+
+
+
+	addRoutes()
+
+	// Let the world know we are alive
+	log.Println("Web starting at", config.Addr)
+	// Go web! Found in web.go
+	log.Println("start web pages at: ", config.Pub)
+	srv.Run(config.Addr, config.Pub)
+}
+
+
+func addRoutes() {
+
+	// Register our REST handler callbacks. For google appengine it needs to
+	// be done here in the init function to ensure the callbacks have been
+	// registered in other modules, if they exist.
 	if config.Pub == "" {
 		srv.Register("/", home)
 	} else {
@@ -41,14 +67,4 @@ func init() {
 	srv.Register("/ws", wserv)
 	srv.Register("/api/health", health)
 	srv.Register("/api/quote", quote)
-}
-
-func main() {
-	flag.Parse()
-
-	// Let the world know we are alive
-	log.Println("Web starting at", config.Addr)
-
-	// Go web! Found in web.go
-	srv.Run(config.Addr, config.Pub)
 }
